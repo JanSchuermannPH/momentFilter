cc1b41d595c7f7a0d62b400e7fa7c1df5c88f535
JanSchuermannPH <jsbaguio.ph@gmail.com>
2016-04-20 22:06:34 +0800

    Cleanup

--- app.js
+++
@@ -1,7 +0,0 @@
-angular.module('testApp',['momentFilter'])
-    // .config(function(momentFilterConfigProvider){
-    //     momentFilterConfigProvider.defaultFormat('dddd, MMMM Do YYYY, h:mm:ss a');
-    // })
-    .controller('testController',['$scope',function($scope){
-        $scope.date = moment();
-    }]);
